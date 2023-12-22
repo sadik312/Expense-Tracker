97496221003366977b006f211da265884d650779
sadik312 <sadikislam46@gmail.com>
2023-12-22 23:35:46 +0000

    corrected typo in comment in Models/Transaction.cs

--- expense-tracker/Models/Transaction.cs
+++ expense-tracker/Models/Transaction.cs
@@ -13,7 +13,7 @@ namespace expense_tracker.Models
         // CategoryId - Primary Key from Category.cs
         public int CategoryId { get; set; }
 
-        // Foreing key relation between this table and category.cs table
+        // Specifying foreign key relation between this table and category.cs table
         public Category Category { get; set; }
         public int Amount { get; set; }
         public string? Note { get; set; }
