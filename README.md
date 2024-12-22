bebae36c308f0de22c9ced644f323a12e9a1cf87
Sadik <sadikislam46@gmail.com>
2024-12-22 13:25:59 +0000

    restarting web app using blazor

--- README.md
+++
@@ -1,5 +0,0 @@
-### Expense Tracker
-
-- Written in C#
-- Using MVC design pattern
-- Syncfusion UI toolkit
--- expense-tracker/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace expense_tracker.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
