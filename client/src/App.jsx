0f75dbe7cdc53eb39c887cc7c12400237a4bc768
sadik <sadikislam46@gmail.com>
2025-01-01 23:14:12 +0000

    created pages folder and HomePage.jsx and rendered it in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './pages/HomePage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <div>
+        <HomePage />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import './HomePage.css';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <h1>HomePage</h1>
+            <p>This is the starting point of the app</p>
+        </div>
+    )
+};
+
+export default HomePage;
