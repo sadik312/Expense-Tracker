88903b1f604b9c0ace753cb091799f6af60c0ec6
Sadik <sadikislam46@gmail.com>
2025-01-02 23:45:19 +0000

    creating a rough outline of what the landing page will look like

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -3,9 +3,13 @@ import '../styles/LandingPage.css';
 
 const LandingPage = () => {
     return (
-        <div className="homepage">
-            <h1>HomePage</h1>
-            <p>This is the starting point of the app</p>
+        <div className="page">
+            <h1 className="heading">Expense Tracker</h1>
+
+            <div className="intro">
+                <h2>Track your spending</h2>
+                <p>Expense Tracker is designed to help you keep track of your spending so you know where your money is going</p>
+            </div>
         </div>
     )
 };
