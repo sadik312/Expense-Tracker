1c58bb73eaebdc869e8f8f3c178bbfccabb64017
Sadik <sadikislam46@gmail.com>
2025-01-02 23:17:01 +0000

    deleted vite.svg and removed import in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './styles/App.css'
-import LandingPage from './pages/LandingPage'
+import { useState } from "react";
+import "./styles/App.css";
+import LandingPage from "./pages/LandingPage";
 
 function App() {
   /* const [count, setCount] = useState(0) */
   return (
-    <LandingPage />
-  )
+    <>
+      <LandingPage />
+    </>
+  );
 }
 
-export default App
+export default App;
 /*
     <>
       <div>
