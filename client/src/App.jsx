66801cbf800a6ef04f0f7f244c6160b27250fed6
Sadik <sadikislam46@gmail.com>
2025-01-04 01:08:05 +0000

    added auth0 authentication and test login button to landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import "./styles/App.css";
 import LandingPage from "./pages/LandingPage";
+import Dashboard from './pages/Dashboard';
 
 function App() {
+  const { isAuthenticated, isLoading } = useAuth0();
+    
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
   return (
     <Router>
       <Routes>
         <Route path='/' element={<LandingPage />} />
+        <Route
+        path='/dashboard'
+        element={isAuthenticated ? <Dashboard /> : <Navigate to='/' />}
+        />
       </Routes>
     </Router>
   );
